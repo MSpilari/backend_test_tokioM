ae854c1939ea32c2ca732031cf1111928defb640
MSpilari <mb.spilari@hotmail.com>
2025-05-20 16:37:02 -0300

    first commit

---
+++ tokio_marine_test/src/main/java/com/mspilari/tokio_marine_test/TokioMarineTestApplication.java
@@ -0,0 +1,13 @@
+package com.mspilari.tokio_marine_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokioMarineTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokioMarineTestApplication.class, args);
+	}
+
+}
